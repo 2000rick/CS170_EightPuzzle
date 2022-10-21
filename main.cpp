22021bf4cf7b8e71cf40723fea6182094cc17362
2000rick <qipeng.liang.edu@gmail.com>
2022-10-20 19:28:03 -0700

    progress, working on queueing function

--- main.cpp
+++ main.cpp
@@ -1,12 +1,28 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
-
 
 class Node {
     public:
+        vector<vector<int>> initial_state;
+        vector<vector<int>> state;
+        vector<pair<int, int>> OPERATORS = {
+             {0, -1}, {0, 1}, {1, 0}, {-1, 0} // left, right, down, up
+        };
+        vector<string> operators_map = {"left", "right", "down", "up"}; 
+        vector<string> moves; // solution moves
+        int n; // size of puzzle (n x n)
+        int cost; // determines the order in queue
+        int search; // search type: 0 = Uniform Cost, 1 = A* w/ Misplaced Tiles, 2 = A* w/ Manhattan Distance
+        bool fail; //denote failure node
+
         Node(int size) {
             n = size;
-            state = vector<vector<int>>(n, vector<int>(n));
+            initial_state = vector<vector<int>>(n, vector<int>(n));
+            state = initial_state;
+            search = 0;
+            cost = 0;
         }
 
         Node(vector<vector<int>> init_state) {
@@ -14,13 +30,7 @@ class Node {
             initial_state = init_state;
             state = init_state;
             search = 0;
-        }
-        
-        Node(const Node &node) {
-            n = node.n;
-            initial_state = node.initial_state;
-            state = node.state;
-            search = 0;
+            cost = 0;
         }
 
         void readPuzzle() {
@@ -74,23 +84,6 @@ class Node {
                 cout << endl;
             }
         }
-
-        void setFail() {
-            fail = true;
-        }
-
-        
-        vector<string> moves; // solution moves
-        vector<vector<int>> initial_state;
-        vector<vector<int>> state;
-        int n; // size of puzzle (n x n)
-        int cost; // determines the order in queue
-        int search; // search type: 0 = Uniform Cost, 1 = A* w/ Misplaced Tiles, 2 = A* w/ Manhattan Distance
-        bool fail; //denote failure node
-        vector<pair<int, int>> OPERATORS = {
-             {0, -1}, {0, 1}, {1, 0}, {-1, 0} // left, right, down, up
-        }; 
-
 };
 
 struct comp {
@@ -98,20 +91,63 @@ struct comp {
 };
 
 typedef priority_queue<Node, vector<Node>, comp> p_queue;
-p_queue MAKE_QUEUE() {
+p_queue MAKE_QUEUE(Node node) {
     priority_queue<Node, vector<Node>, comp> nodes;
+    nodes.push(node);
     return nodes;
 }
 
-Node general_search(Node &problem) {
-    queue<Node> nodes;
+Node MAKE_NODE(vector<vector<int>> &initial_state) {
+    Node node(initial_state);
+    return node;
+}
 
+Node REMOVE_FRONT(p_queue &nodes) {
+    Node node = nodes.top();
+    nodes.pop();
+    return node;
+}
+
+bool valid_index(int r, int c, int n) {
+    return (r >= 0 && c >= 0 && r < n && c < n);
+}
+
+void QUEUEING_FUNCTION(p_queue &nodes, const Node &node, const vector<pair<int, int>> &OPERATORS) {
+    int rz=0, cz=0; // row and column of zero (aka the blank tile)
+    for(int i=0; i<node.n; ++i) {
+        for(int j=0; j<node.n; ++j) {
+            if(node.state[i][j] == 0) {
+                rz = i;
+                cz = j;
+            }
+        }
+    }
+
+    for (int i=0; i < OPERATORS.size(); ++i) {
+        int r = rz + OPERATORS[i].first;
+        int c = cz + OPERATORS[i].second;
+        if (valid_index(r, c, node.n)) {
+            Node expanded = node;
+            swap(expanded.state[rz][cz], expanded.state[r][c]); // equivalent to moving the blank tile to the new position
+            expanded.moves.push_back(expanded.operators_map[i]);
+            expanded.cost += 1;
+            nodes.push(expanded);
+        }
+    }
+}
+
+Node general_search(Node &problem) {
+    p_queue nodes = MAKE_QUEUE(MAKE_NODE(problem.initial_state));
     while(true) {
         if(nodes.empty()) {
             Node failure(1);
-            failure.setFail();
+            failure.fail = true;
             return failure;
         }
+
+        Node node = REMOVE_FRONT(nodes);
+        if(node.GOAL_STATE()) return node;
+
     }
 
     return problem;
@@ -141,8 +177,9 @@ int main() {
     if(result.fail) {
         cout << "FAILURE" << endl;
     }
-
-    MAKE_QUEUE();
+    else {
+        cout << "SUCCESS" << endl;
+    }
 
     return 0;
 }
